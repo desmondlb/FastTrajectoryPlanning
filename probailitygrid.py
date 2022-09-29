dab2c095308b1a4782987ef7d34c1f9cb5afdf88
jhanaviB <48366761+jhanaviB@users.noreply.github.com>
2022-09-29 19:16:39 -0400

    Delete probailitygrid.py

--- probailitygrid.py
+++
@@ -1,7 +0,0 @@
-import random
-import numpy as np
-nums = [0,1]
-grid = [[random.choices(nums, weights=(70,30), k=2500) for x in range(50)] for y in range(50)]
-print(grid)
-#Assuming 1 as unblocked
-
