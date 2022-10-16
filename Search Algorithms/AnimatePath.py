539a808b8d9288d65776cafb007d8b3a130b44c7
TariDi <38293809+TariDi@users.noreply.github.com>
2022-10-15 20:02:16 -0400

    Add files via upload
    Class to animate the shortest presumed unblocked paths returned by the various A* algorithms

---
+++ Search Algorithms/AnimatePath.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+class AnimatePath:
+
+    def __init__(self, grid=None, path=None, start=None,
+                 target=None) -> None:
+        self.grid = grid
+        self.path = path
+        self.start = start
+        self.target = target
+
+
+    def animate(self, i):
+        im = plt.imshow(self.grid, cmap="RdBu")
+        point = self.path[i]
+        self.grid[point[0]][point[1]] = 4
+        im.set_array(self.grid)
+        return [im]
+
+    def show_path(self):
+        fig = plt.figure()
+        self.grid[self.start[0]][self.start[1]] = 3
+        self.grid[self.target[0]][self.target[1]] = 5
+        anim = FuncAnimation(fig, self.animate, interval=10, frames=len(self.path), blit=True)
+        plt.show()
+
